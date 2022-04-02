2ecaa15a0421f6396da7cd907eb1075cac79287f
jhhg04 <jhhg04@gmail.com>
2022-04-02 13:10:36 -0500

    first commit

---
+++ src/init.js
@@ -0,0 +1,10 @@
+const config = {
+  width: 800,
+  height: 600,
+  parent: 'container',
+  type: Phaser.CANVAS,
+  backgroundColor: '#392542',
+  scene: [SceneA, SceneB],
+};
+
+new Phaser.Game(config);
